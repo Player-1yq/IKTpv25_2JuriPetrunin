48ed848640eb3262c4985fb14999d3b641cfe1ae
Player-1yq <petrunin.j@gmail.com>
2025-12-12 17:11:16 +0200

    Implement random number generation and categorization
    The function generates a list of random integers, sorts them, and categorizes them into positive, negative, and zero values. It also calculates and adds the averages of positive and negative numbers back into the list.

--- IKTpv25_2JuriPetrunin/module1.py
+++ IKTpv25_2JuriPetrunin/module1.py
@@ -1,4 +1,55 @@
-from module1 import* 
+from random import *
+
+#Põhifunktsioon
+def arvud_loendis():
+    """Kirjandus
+
+        Küsib, kui palju arve genereerida.
+        Küsib juhuarvude vahemiku.
+        Genereerib juhuslike täisarvude loendi.
+        Sorteerib selle täisarvu.
+        Jagab loendi kolmeks: positiivsed, negatiivsed ja nullid.
+        Leiab positiivsete ja negatiivsete keskmised.
+        Lisab need keskmised algsesse loendisse.
+        Sorteerib ja väljastab lõpliku loendi.
+    """
+    print("Andmed:")
+    try:
+        n=abs(int(input("Mitu täisarvu genereerime loendisse? => ")))
+        mini=int(input("Sisesta vahemiku minimaalne arv => "))
+        maxi=int(input("Sisesta vahemiku maksimaalne arv => "))
+    except ValueError:
+        print("Sisestake ainuld arvu")
+
+    if mini>=maxi:
+        mini,maxi =vahetus(mini,maxi)
+
+    #vaja kustuda ":", kus nad olid
+    s=[]
+    pos=[]
+    neg=[]
+    null=[]
+    generaator(n,s,mini,maxi)#tabulatsioon
+    print()
+    print("Tulemused: ")
+    print("Saadud loend alates",mini,"kuni",maxi,s)
+    s.sort() #s.sort
+    print("Sorteeritud loend",s)
+    jagamine(s,pos,neg,null)
+    print("Positiivsete elementide loend",pos)
+    print("Negatiivsete elementide loend",neg)
+    print("Null-elementide loend",null)
+    kesk=keskmine(pos)
+    lisamine(s,kesk)
+    print("Positiivsete keskmine:",kesk)
+    kesk=keskmine(neg)
+    lisamine(s,kesk)
+    print("Negatiivsete keskmine:",kesk)
+    print("Lisame keskmised algsesse massiivi:")
+    s.sort() # s.sort
+    print(s)
+   
+
 
 #Põhifunktsiooni käivitus
 arvud_loendis()
